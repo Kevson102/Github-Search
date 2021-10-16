73c7971e2291a3ea834489881a3ef500bff01cb1
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-10-16 23:03:56 +0300

    DISPLAYS THE LIST OF REPOSITORIES FOR THE HARDCODED USER

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SearchService } from './services/search/search.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'GithubSearch';
+  username: string = 'Kevson102';
+  repositories: any[] = [];
+  
+  constructor(private searchSerivce: SearchService){
+
+  }
+  ngOnInit(): void { 
+    this.getRepositories();
+  }
+
+  getRepositories(){
+    this.searchSerivce.getRepositories(this.username).subscribe((repos)=>{
+      // console.log(response)
+      this.repositories = repos;
+    })
+  }
 }
--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -1,14 +1,45 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  private baseURL = environment.baseURL;
 
   constructor(private httpClient:HttpClient) { }
 
-  getRepository(){
+  // getRepository(){
     
-  }
+  // }
+  // repositoryRequest(){
+  //   interface ApiResponse{
+  //     name:string;
+  //     description:string;
+  //   }
+
+  getRepositories(username:string): Observable<any[]> {
+    // let param = new HttpParams();
+    // param = param.append('username', 'SamTomashi');
+    return this.httpClient.get<any[]>(`${this.baseURL}users/${username}/repos`, {
+      // params: param,
+      // // headers : this.headers,
+      // withCredentials: true
+    });
+   }
+  
 }
+
+
+
+// getUsers(): Observable<GithubApi[]> {
+//   let param = new HttpParams();
+//   param = param.append('username', 'SamTomashi');
+//   return this.httpClient.get<GithubApi[]>(`https://api.github.io/users`, {
+//     params: param,
+//     // headers : this.headers,
+//     withCredentials: true
+//   });
+//  }
