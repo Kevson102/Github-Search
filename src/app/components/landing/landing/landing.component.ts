9a41b143c837072feadcf686ce8d902a561e873b
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-10-17 14:59:08 +0300

    CHAINS THE PIPE TO CALCULATE THE NUMBER OF DAYS ELAPSED SINCE THE WEB APP WAS PUBLISHED BASED ON A HARD-CODED DATE

--- src/app/components/landing/landing/landing.component.ts
+++ src/app/components/landing/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/user/user';
 
 @Component({
   selector: 'app-landing',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+  users:User[] = [
+    new User(new Date(2021,10,17)),
+  ]
 
   constructor() { }
 
