a4faa1b23de8b6ae10ee78fd927cc9d67d310fdf
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-10-17 05:10:56 +0300

    GENERATES THE NAVBAR COMPONENT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing/landing.component';
 import { CentralComponent } from './central/central.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    CentralComponent
+    CentralComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
