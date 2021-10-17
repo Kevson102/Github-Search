4b041c20e135a2391a8601198b615745d1622f21
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-10-17 06:22:02 +0300

    EDITS THE SEARCH PAGE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CentralComponent } from './central/central.component';
 import { LandingComponent } from './components/landing/landing/landing.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {path:'landing', component: LandingComponent},
   {path: 'central', component: CentralComponent},
   { path: '', redirectTo:"landing", pathMatch:"full"},
+  { path:'**', component: NotFoundComponent},
   
 ];
 
