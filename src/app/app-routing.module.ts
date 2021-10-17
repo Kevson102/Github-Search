24261771bc54235d26bab663301857fbdbb3ac15
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-10-17 05:09:21 +0300

    ADDS CODE IN THE ROUTING MODULE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CentralComponent } from './central/central.component';
+import { LandingComponent } from './components/landing/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'landing', component: LandingComponent},
+  {path: 'central', component: CentralComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
